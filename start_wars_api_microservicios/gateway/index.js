8bcde837835d553d09070fe6dd8bb5a5c71c144d
jerlibgnzlz <jerlibgv@gmail.com>
2023-09-18 19:55:55 -0300

    add gateways

--- start_wars_api_microservicios/gateway/index.js
+++ start_wars_api_microservicios/gateway/index.js
@@ -8,18 +8,23 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 
-app.use("/characters", createProxyMiddleware({
-  target: "http://localhost:8001",
-  changeOrigin: true,
-}));
-app.use("/films", createProxyMiddleware({
-  target: "http://localhost:8002",
-  changeOrigin: true,
-}));
-app.use("/planets", createProxyMiddleware({
-  target: "http://localhost:8003",
-  changeOrigin: true,
-}));
+app.use("/characters",
+  createProxyMiddleware({
+    target: "http://localhost:8001",
+    changeOrigin: true,
+  }));
+
+app.use("/films",
+  createProxyMiddleware({
+    target: "http://localhost:8002",
+    changeOrigin: true,
+  }));
+
+app.use("/planets",
+  createProxyMiddleware({
+    target: "http://localhost:8003",
+    changeOrigin: true,
+  }));
 
 
 const PORT = 8000;
