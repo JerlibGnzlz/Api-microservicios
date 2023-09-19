dab7414369a8d6f91cc33bf8830b5f71523bfeb6
jerlibgnzlz <jerlibgv@gmail.com>
2023-09-19 20:11:54 -0300

    add model planet y config de database

--- start_wars_api_microservicios/database/src/database/index.js
+++ start_wars_api_microservicios/database/src/database/index.js
@@ -4,7 +4,18 @@ const { MONGO_URI } = require("../config/envs");
 
 const connect = mongoose.createConnection(MONGO_URI);
 
-const Character = connect.model("characters", require("./models/CharacterModels"));
+// const Character = connect.model("characters", require("./models/CharacterModels"));
+// const films = connect.model("films", require("./models/filmsModels"));
+// const planet = connect.model("planets", require("./models/planetsModels"));
 
-Character.find()
-  .then((res) => console.log(res));
+// Character.find()
+//   .populate("films", ["_id", "title"])
+//   .then((res) => console.log(res[0]));
+
+
+
+module.exports = {
+  Character: connect.model("characters", require("./models/CharacterModels")),
+  films: connect.model("films", require("./models/filmsModels")),
+  planet: connect.model("planets", require("./models/planetsModels"))
+};
