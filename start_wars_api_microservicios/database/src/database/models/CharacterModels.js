b64eca4607bdf6b31def3272d38e2acfd8be250b
jerlibgnzlz <jerlibgv@gmail.com>
2023-09-19 04:11:31 -0300

    add database & model

---
+++ start_wars_api_microservicios/database/src/database/models/CharacterModels.js
@@ -0,0 +1,30 @@
+const { Schema } = require("mongoose");
+
+const characterSchema = new Schema(
+  {
+    _id: String,
+    name: String,
+    height: String,
+    mass: String,
+    hair_color: String,
+    skin_color: String,
+    eye_color: String,
+    birth_year: String,
+    gender: String,
+    homeworld: {
+      type: String,
+      ref: "Planet"
+    },
+    films: [{
+      type: String,
+      ref: "film"
+    }]
+    //   ,
+    //   {
+    //   versionKey: false,
+    //   timestamps: false
+    // });
+  });
+
+
+module.exports = characterSchema;
