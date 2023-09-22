8a2148e25cae5ecc3f758e77537b380327c4cd5a
jerlibgnzlz <jerlibgv@gmail.com>
2023-09-22 15:30:58 -0300

    add axios a los metodos list

--- start_wars_api_microservicios/characters/src/data/index.js
+++ start_wars_api_microservicios/characters/src/data/index.js
@@ -1,8 +1,12 @@
 const characters = require("../data/characters.json");
+const axios = require("axios");
+
 
 module.exports = {
   list: async () => {
-    return characters;
+    // return characters;
+    let characters = await axios("http://database:8004/Character");
+    return characters.data;
   },
   create: async () => {
     throw Error("No se puede crear un personaje");
--- start_wars_api_microservicios/planets/src/data/index.js
+++ start_wars_api_microservicios/planets/src/data/index.js
@@ -2,7 +2,9 @@ const planets = require("../data/planets.json");
 
 module.exports = {
   list: async () => {
-    return planets;
+    let planets = await axios("http://database:8004/Planets");
+    return planets.data;
+
   }
 
 
